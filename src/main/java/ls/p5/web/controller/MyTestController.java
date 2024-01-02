98635d0e6230f198baa5f746297e88df03cb1a2d
Vikas Rajaura <rajauravikas@gmail.com>
2024-01-02 11:54:05 +0530

    renamed main to index, few ui changes

--- src/main/java/ls/p5/web/controller/MyTestController.java
+++ src/main/java/ls/p5/web/controller/MyTestController.java
@@ -1,17 +1,22 @@
 package ls.p5.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyTestController {
 
-    @GetMapping("/")
-    public String test(){
-        return "index";
+    @GetMapping("/courses")
+    public String courses(){
+        return "courses";
     }
 
-
+    @GetMapping("/")
+    public String main(Model model){
+        model.addAttribute("msg", "Hi welcome to Thymeleaf");
+        return "index";
+    }
 
 }
